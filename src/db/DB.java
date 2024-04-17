b98aa8b142b57621d736e2e7cd79a473fbe31f85
Levelvini <viniciusfc29@gmail.com>
2024-04-17 11:03:32 -0300

    added properties reader

--- src/db/DB.java
+++ src/db/DB.java
@@ -1,4 +1,19 @@
 package db;
 
+import java.io.*;
+import java.util.Properties;
+
 public class DB {
+
+
+
+    private static Properties loadProperties(){
+        try (FileInputStream fs = new FileInputStream("db.properties")){
+            Properties props = new Properties();
+            props.load(fs);
+            return props;
+        }catch (IOException e){
+            throw new DBException(e.getMessage());
+        }
+    }
 }
