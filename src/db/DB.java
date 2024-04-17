785b4e418635c637d2469ffe255410c10e9c432e
Levelvini <viniciusfc29@gmail.com>
2024-04-17 15:43:29 -0300

    "class program added"

--- src/db/DB.java
+++ src/db/DB.java
@@ -1,11 +1,34 @@
 package db;
 
 import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class DB {
 
-
+    private static Connection conn = null;
+    public static Connection getConnection(){
+        if (conn == null){
+            try {
+            Properties props = loadProperties();
+            String url = props.getProperty("dburl");
+            conn = DriverManager.getConnection(url,props);
+            }catch (SQLException e){
+                throw new DBException(e.getMessage());
+            }
+        }return conn;
+    }
+    public static void closeConnection(){
+        try {
+        if (conn != null){
+            conn.close();
+          }
+        }catch (SQLException e){
+            throw new DBException(e.getMessage());
+        }
+    }
 
     private static Properties loadProperties(){
         try (FileInputStream fs = new FileInputStream("db.properties")){
